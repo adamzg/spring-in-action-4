46b7837067d2a7a56cb8db49a94e20e609f62a7f
Adam Gresh <adam.gresh@modmed.com>
2016-05-05 10:46:05 -0400

    adjust tests to work with adjusted location of soundsystem.xml

--- test/soundsystem/CDPlayerTest.java
+++ test/soundsystem/CDPlayerTest.java
@@ -10,7 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//@RunWith bootstraps spring.
+//How would one do this from a main method?
+//How would one do this from a web application?
 @RunWith(SpringJUnit4ClassRunner.class)
+//@ContextsConfiguration tells spring to scan CDPlayerConfig for 
+//spring configuration annotations
 @ContextConfiguration(classes=CDPlayerConfig.class)
 public class CDPlayerTest {
 
